7dd872e6af790bcc0c8787b9cd05c8816bd0b8a0
BrionesAngel <14tenshikun14@gmail.com>
2026-07-22 00:03:55 -0600

    feat/backend showtime entity class created

---
+++ backend/src/main/java/com/example/backend/features/showtimeseats/ShowtimeSeat.java
@@ -0,0 +1,36 @@
+package com.example.backend.features.showtimeseats;
+
+import com.example.backend.features.reservations.Reservation;
+import com.example.backend.features.seats.Seat;
+import com.example.backend.features.showtimes.Showtime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "showtime_seats")
+public class ShowtimeSeat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private ShowtimeSeatStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "showtime_id", nullable = false)
+    private Showtime showtime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id", nullable = false)
+    private Seat seat;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+}
---
+++ backend/src/main/java/com/example/backend/features/showtimeseats/ShowtimeSeatStatus.java
@@ -0,0 +1,8 @@
+package com.example.backend.features.showtimeseats;
+
+public enum ShowtimeSeatStatus {
+  AVAILABLE,
+  RESERVED,
+  BOOKED,
+  BLOCKED
+}
