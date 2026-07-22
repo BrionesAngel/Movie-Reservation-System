5d5460404a1af7a82cb8307c9ef44e537441fa88
BrionesAngel <14tenshikun14@gmail.com>
2026-07-22 00:01:49 -0600

    feat/db create movies migration added

---
+++ backend/src/main/resources/db/migration/V2__create_movies.sql
@@ -0,0 +1,7 @@
+CREATE TABLE movies (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(150) NOT NULL,
+  description TEXT NOT NULL,
+  duration INT NOT NULL,
+  poster_url VARCHAR(255) NOT NULL
+);
