3a84bd902209ecfd8c8e9b5ceceb1c9b82c0e9dd
BrionesAngel <14tenshikun14@gmail.com>
2026-07-29 23:14:31 -0600

    feat/backend/migration-refactor

--- backend/src/main/resources/db/migration/V10__create_showtime_seats.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE showtime_seats (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  status VARCHAR(15) NOT NULL,
-  showtime_id BIGINT NOT NULL REFERENCES showtimes(id),
-  seat_id BIGINT NOT NULL REFERENCES seats(id),
-  reservation_id BIGINT REFERENCES reservations(id)
-);
