44d66d74604c3fab76a33e849539802b2f6457fb
BrionesAngel <14tenshikun14@gmail.com>
2026-09-04 00:16:05 -0600

    fix: optimize showtime seat query

--- backend/src/main/java/com/example/backend/features/showtime_seats/ShowtimeSeatRepository.java
+++ backend/src/main/java/com/example/backend/features/showtime_seats/ShowtimeSeatRepository.java
@@ -3,8 +3,15 @@ package com.example.backend.features.showtime_seats;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShowtimeSeatRepository extends JpaRepository<ShowtimeSeat, Long> {
+  @Query("""
+          SELECT ss
+          FROM ShowtimeSeat ss
+          JOIN FETCH ss.seat
+          WHERE ss.showtime.id = :showtimeId
+      """)
   List<ShowtimeSeat> findAllByShowtimeId(Long showtimeId);
 
   List<ShowtimeSeat> findAllByShowtimeIdAndIdIn(Long showtimeId, List<Long> ids);
