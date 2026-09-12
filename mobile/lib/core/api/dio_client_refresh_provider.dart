0935a3a712dea6e70fca464b6ce0bf771b8df06d
BrionesAngel <14tenshikun14@gmail.com>
2026-09-11 23:55:12 -0600

    refactor: simplify refresh dio into single provider

--- mobile/lib/core/api/dio_client_refresh_provider.dart
+++ mobile/lib/core/api/dio_client_refresh_provider.dart
@@ -2,8 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dioClientRefreshProvider = Provider<Dio>((ref) {
-
-  final dio = Dio(
+  return Dio(
     BaseOptions(
       baseUrl: 'http://localhost:8080/api',
       connectTimeout: const Duration(seconds: 10),
@@ -12,7 +11,4 @@ final dioClientRefreshProvider = Provider<Dio>((ref) {
       headers: {'Content-Type': 'application/json'},
     ),
   );
-
-
-  return dio;
 });
